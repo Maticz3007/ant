8066b810e3068f8e2198ad67938b13685052e879
Mateusz <mateusz.czyzewski04@outlook.com>
2023-12-23 18:09:37 +0100

    Prototyp wyprowadzenia planszy do pliku (na razie ostatni stan), uporządkowanie kodu

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-void field_change(int* y, int* x, int size_r, int size_c, char siatka[size_r][size_c], int* ptr)
+void zmiana_pola(int* y, int* x, int size_r, int size_c, char siatka[size_r][size_c], int* ptr)
 {
     if(siatka[*y][*x]=='.')
     {
@@ -14,7 +14,6 @@ void field_change(int* y, int* x, int size_r, int size_c, char siatka[size_r][si
             case 2: *ptr = 3; *x=*x-1; break;
             case 3: *ptr = 0; *y=*y-1; break;
         }
-
     }
     else
     if(siatka[*y][*x]=='X')
@@ -29,36 +28,8 @@ void field_change(int* y, int* x, int size_r, int size_c, char siatka[size_r][si
         }
     }
 }
-
-
-int main(int argc, char *argv[])
+void wypisz_siatke(int size_r, int size_c, char siatka[size_r][size_c])
 {
-    int size_r = atoi(argv[1]);
-    if(size_r==0) return -1;
-    int size_c = atoi(argv[2]);
-    if(size_c==0) return -2;
-    char siatka[size_r][size_c];
-    int ant_r = atoi(argv[3]);
-    int *r = &ant_r;
-    int ant_c = atoi(argv[4]);
-    int *c = &ant_c;
-    for(int i=0; i<size_r; i++)
-    {
-        for(int j=0; j<size_c; j++)
-        {
-            siatka[i][j] = '.';
-            //printf("%c", siatka[i][j]);
-        }
-        //printf("\n");
-    }
-    int ant_state = atoi(argv[5]);  //0 = U - up, 1 = R - right, 2 = D - down, 3 = L - left
-    int *p = &ant_state;
-    field_change(r, c, size_r, size_c, siatka, p);
-    field_change(r, c, size_r, size_c, siatka, p);
-    field_change(r, c, size_r, size_c, siatka, p);
-    field_change(r, c, size_r, size_c, siatka, p);
-
-    printf("%d\n", ant_state);
     for(int i=0; i<size_r; i++)
     {
         for(int j=0; j<size_c; j++)
@@ -67,5 +38,46 @@ int main(int argc, char *argv[])
         }
         printf("\n");
     }
+}
+
+int main(int argc, char *argv[])
+{
+    int size_r = atoi(argv[1]);
+    if(size_r<1) return -1;
+    int size_c = atoi(argv[2]);
+    if(size_c<1) return -2;
+    char siatka[size_r][size_c];
+    int ant_r = atoi(argv[3]);
+    int *r = &ant_r;
+    int ant_c = atoi(argv[4]);
+    int *c = &ant_c;
+    int ant_state = atoi(argv[5]);  //0 = U - up, 1 = R - right, 2 = D - down, 3 = L - left
+    int *p = &ant_state;
+
+    for(int i=0; i<size_r; i++)
+        for(int j=0; j<size_c; j++)
+            siatka[i][j] = '.';
+
+    for(int i=0; i<atoi(argv[6]); i++)
+    {
+        zmiana_pola(r, c, size_r, size_c, siatka, p);
+        wypisz_siatke(size_r, size_c, siatka);
+    }
+
+    FILE *fptr;
+    fptr = fopen("C:\\ant_output\\test.txt","w"); //w makefile należy dać mkdir
+    if(fptr == NULL)
+    {
+        printf("Error!");
+        return -10;
+    }
+    for(int i=0; i<size_r; i++)
+        {
+            for(int j=0; j<size_c; j++)
+            {
+                putc(siatka[i][j], fptr);
+            }
+            fprintf(fptr, "\n");
+        }
     return 0;
 }
